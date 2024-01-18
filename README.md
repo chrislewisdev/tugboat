86cd0646b0707ad9bfa8269585a8a94a8a6df26d
Chris Lewis-Hou <Chris.Lewis-Hou@ratemyagent.com>
2024-01-19 00:20:12 +1100

    Add basic file input to cli

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 `tgbl` is a **T**oy **G**ame**B**oy **L**anguage. This is a hobby project to create a basic language that can be compiled and run on a Gameboy. It is not intended for production usage!
+
+## Example
+
+```
+u8 variable;
+
+fn main() {
+    variable = 5;
+}
+```
--- tgbl_cli/src/main.rs
+++ tgbl_cli/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, ValueEnum};
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum Format {
+    Rgbds,
+    Raw,
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    // Files to compile
+    #[arg(required = true)]
+    file: PathBuf,
+    // Compiled output format
+    #[arg(long)]
+    format: Option<Format>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let file = fs::read_to_string(&args.file);
+    match file {
+        Err(err) => println!("Unable to open {:?}: {}", args.file, err),
+        Ok(_contents) => println!("Compiling {:?}", args.file),
+    }
 }
